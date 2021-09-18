5a4b01c6f9c594aa1478cb06936664dcb4740c9e
daku10 <daku.two@gmail.com>
2021-09-18 21:39:53 +0900

    0039 TimeLimitExceeded

--- 0039-Combination-Sum/main.go
+++ 0039-Combination-Sum/main.go
@@ -1,5 +1,69 @@
 package main
 
+import (
+	"fmt"
+	"sort"
+)
+
+
 func combinationSum(candidates []int, target int) [][]int {
-    return nil
+	itemMap := make(map[int][][]int)
+	resultMap := make(map[string]bool)
+	return search(candidates, target, itemMap, resultMap)
+}
+
+func search(candidates []int, num int, itemMap map[int][][]int, resultMap map[string]bool) [][]int {
+	if itemMap[num] != nil {
+		return itemMap[num]
+	}
+
+	result := make([][]int, 0)
+
+	length := len(candidates)
+	for i := 0; i < length; i++ {
+		candidate := candidates[i]
+		if candidate == num {
+			r := []int{num}
+			result = append(result, r)
+			if itemMap[num] == nil {
+				tmp := make([][]int, 0)
+				tmp = append(tmp, r)
+				itemMap[num] = tmp
+			} else {				
+				itemMap[num] = append(itemMap[num], r)
+			}		
+		}
+	}
+
+
+	for i := 1; i <= num / 2; i++ {
+		n1 := i
+		r1 := search(candidates, n1, itemMap, resultMap)
+		n2 := num - i
+		r2 := search(candidates, n2, itemMap, resultMap)
+		for j := 0; j < len(r1); j++ {
+			for k := 0; k < len(r2); k++ {
+				rj := r1[j]
+				rk := r2[k]
+				tmp := make([]int, 0)
+				tmp = append(tmp, rj...)
+				tmp = append(tmp, rk...)
+				sort.IntSlice(tmp).Sort()
+				key := fmt.Sprintf("%d_%v", num, tmp)
+				if !resultMap[key] {
+					result = append(result, tmp)
+					if itemMap[num] == nil {
+						im := make([][]int, 0)
+						im = append(im, tmp)
+						itemMap[num] = im
+					} else {				
+						itemMap[num] = append(itemMap[num], tmp)
+					}
+					resultMap[key] = true
+				} 
+			}
+		}
+	}
+
+	return result
 }
--- 0039-Combination-Sum/main_test.go
+++ 0039-Combination-Sum/main_test.go
@@ -16,9 +16,10 @@ func TestCombinationSum(t *testing.T) {
 
 	testCases := []TestCase{
 		{[]int{2, 3, 5}, 8, [][]int{{2, 2, 2, 2},{2, 3, 3}, {3, 5}}},
-		{[]int{2}, 1, [][]int{{}}},
+		{[]int{2}, 1, [][]int{}},
 		{[]int{1}, 1, [][]int{{1}}},
 		{[]int{1}, 2, [][]int{{1, 1}}},
+		{[]int{48,22,49,24,26,47,33,40,37,39,31,46,36,43,45,34,28,20,29,25,41,32,23}, 69, [][]int{}},
 	}
 
 	for index, testCase := range testCases {
