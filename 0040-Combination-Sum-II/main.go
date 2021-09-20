22daf929eae60f9e5bbccddeb852a55c9ec42f2f
daku10 <daku.two@gmail.com>
2021-09-20 19:33:50 +0900

    0040 write some logic

--- 0040-Combination-Sum-II/main.go
+++ 0040-Combination-Sum-II/main.go
@@ -1,5 +1,17 @@
 package main
 
+import "fmt"
+
 func combinationSum2(candidates []int, target int) [][]int {
+
+	candidateMap := make(map[int]int)
+	length := len(candidates)
+	for i := 0; i < length; i++ {
+		candidateMap[candidates[i]]++
+	}
+
+	fmt.Println(candidateMap)
+
 	return nil
 }
+
