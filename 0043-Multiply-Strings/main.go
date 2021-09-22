133357e13d18cbdd6ea41c78c832b753961d6a5e
daku10 <daku.two@gmail.com>
2021-09-22 22:25:21 +0900

    0043 add some codes...

--- 0043-Multiply-Strings/main.go
+++ 0043-Multiply-Strings/main.go
@@ -1,5 +1,16 @@
 package main
 
 func multiply(num1 string, num2 string) string {
+    if num1 == "0" || num2 == "0" {
+        return "0"
+    }
+
+    len1 := len(num1)
+    len2 := len(num2)
+    for i := 0; i < len1; i++ {
+        for j := 0; j < len2; j++ {
+            
+        }
+    }
     return ""
 }
