96b4c4cf05a84d6bb994386379af40fc8917c2c8
daku10 <daku.two@gmail.com>
2021-09-25 23:31:01 +0900

    0045 add testcase

---
+++ 0045-Jump-Game-II/main_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestJump(t *testing.T) {
+
+	type TestCase struct {
+		arg []int
+		expect int
+	}
+
+	testCases := []TestCase{
+		{[]int{2, 3, 1, 1, 4}, 2},
+		{[]int{2, 3, 0, 1, 4}, 2},
+	}
+
+	for index, testCase := range testCases {
+		t.Run(fmt.Sprint(index), func(t *testing.T) {
+			actual := jump(testCase.arg)
+			if actual != testCase.expect {
+				t.Errorf("nums: %v, expect :%d, actual :%d", testCase.arg, testCase.expect, actual)
+			}
+		})
+	}
+}
