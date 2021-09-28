4fddca929d0ccff283fedf718c1db11f6b18d562
daku10 <daku.two@gmail.com>
2021-09-28 21:29:21 +0900

    finish 0046

--- 0046-Permutations/main.go
+++ 0046-Permutations/main.go
@@ -1,5 +1,42 @@
 package main
 
 func permute(nums []int) [][]int {
-    return nil
+
+	length := len(nums)
+
+	result := make([][]int, 0)
+
+	for i := 0; i < length; i++ {
+		n := nums[i]
+		rest := make([]int, 0)
+		for j := 0; j < length; j++ {
+			if n != nums[j] {
+				rest = append(rest, nums[j])
+			}
+		}
+		inner([]int{n}, rest, &result)
+	}
+
+    return result
+}
+
+func inner(current []int, rest []int, result *[][]int) {
+	lenRest := len(rest)
+	if lenRest == 0 {
+		*result = append((*result), current)
+		return
+	}
+	for i := 0; i < lenRest; i++ {
+		r := rest[i]
+		restRest := make([]int, 0)
+		for j := 0; j < lenRest; j++ {
+			if r != rest[j] {
+				restRest = append(restRest, rest[j])
+			}
+		}
+		newCurrent := make([]int, 0)
+		newCurrent = append(newCurrent, current...)
+		newCurrent = append(newCurrent, r)
+		inner(newCurrent, restRest, result)
+	}
 }
--- 0046-Permutations/main_test.go
+++ 0046-Permutations/main_test.go
@@ -17,6 +17,7 @@ func TestPermute(t *testing.T) {
 		{[]int{1,2,3}, [][]int{{1,2,3}, {1,3,2}, {2,1,3}, {2,3,1}, {3,1,2}, {3,2,1}}},
 		{[]int{0,1}, [][]int{{0,1}, {1,0}}},
 		{[]int{1}, [][]int{{1}}},
+		{[]int{-1, 2}, [][]int{{-1, 2}, {2, -1}}},
 	}
 
 	for index, testCase := range testCases {
