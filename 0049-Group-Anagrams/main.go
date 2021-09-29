8882951930c64f9e776d7ef01d43e8e2f446e439
daku10 <daku.two@gmail.com>
2021-09-29 23:12:08 +0900

    0049 TimeLimitExceeded

--- 0049-Group-Anagrams/main.go
+++ 0049-Group-Anagrams/main.go
@@ -1,5 +1,48 @@
 package main
 
+import (
+	"reflect"
+	"sort"
+	"strings"
+)
+
 func groupAnagrams(strs []string) [][]string {
-    return nil
+
+	result := make([][]string, 0)
+
+	length := len(strs)
+
+	memo := make(map[string]bool)
+
+	for i := 0; i < length; i++ {
+		str := strs[i]
+
+		if memo[str] {
+			continue
+		}
+
+		res := []string{str}
+		
+		for j := i + 1; j < length; j++ {
+			compareStr := strs[j]
+			if isAnagram(str, compareStr) {
+				memo[compareStr] = true
+				res = append(res, compareStr)
+			}
+		}
+		result = append(result, res)
+	}
+
+    return result
+}
+
+func isAnagram(l string, r string) bool {
+	if len(l) != len(r) {
+		return false
+	}
+	lSplit := strings.Split(l, "")
+	sort.Strings(lSplit)
+	rSplit := strings.Split(r, "")
+	sort.Strings(rSplit)
+	return reflect.DeepEqual(lSplit, rSplit)
 }
