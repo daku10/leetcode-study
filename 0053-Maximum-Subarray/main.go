35ef2f808ec9e5bfc59235830d1c56a94a830331
daku10 <daku.two@gmail.com>
2022-04-05 20:14:17 +0900

    0053 remove redundant variable

--- 0053-Maximum-Subarray/main.go
+++ 0053-Maximum-Subarray/main.go
@@ -6,22 +6,14 @@ import (
 
 func maxSubArray(nums []int) int {
 	result := math.MinInt
-	currentResult := 0
+	currentResult := math.MinInt
 	length := len(nums)
-	isSearchFirst := true
 	for i := 0; i < length; i++ {
-		if isSearchFirst {
-			currentResult = nums[i]
-			isSearchFirst = false
+		tmp := nums[i]
+		if currentResult < 0 && tmp > currentResult {
+			currentResult = tmp
 		} else {
-			tmp := nums[i]
-			if currentResult < 0 && tmp > currentResult {
-				currentResult = tmp
-			} else if currentResult+tmp < 0 {
-				isSearchFirst = true
-			} else {
-				currentResult += tmp
-			}
+			currentResult += tmp
 		}
 		if currentResult > result {
 			result = currentResult
