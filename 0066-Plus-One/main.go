b7f1e22f5a38ae82a0e3a191770ce2fd2b184191
daku10 <daku.two@gmail.com>
2022-04-09 21:48:05 +0900

    0066 improve performance, but modify parameter

--- 0066-Plus-One/main.go
+++ 0066-Plus-One/main.go
@@ -2,22 +2,20 @@ package main
 
 func plusOne(digits []int) []int {
 	length := len(digits)
-	result := make([]int, length)
-	copy(result, digits)
-	result[length-1] += 1
+	digits[length-1] += 1
 	carryOver := false
 	for i := length - 1; i >= 0; i-- {
 		if carryOver {
-			result[i] += 1
+			digits[i] += 1
 			carryOver = false
 		}
-		if result[i] == 10 {
-			result[i] = 0
+		if digits[i] == 10 {
+			digits[i] = 0
 			carryOver = true
 		}
 	}
 	if carryOver {
-		result = append([]int{1}, result...)
+		return append([]int{1}, digits...)
 	}
-	return result
+	return digits
 }
