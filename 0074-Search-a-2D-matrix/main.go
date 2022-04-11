e929f77c3f194d2b9a3373fbff55fcf2751fd575
daku10 <daku.two@gmail.com>
2022-04-11 18:26:17 +0900

    0074 write faster algorithm potentially, but data size is not suitable for the algorithm

--- 0074-Search-a-2D-matrix/main.go
+++ 0074-Search-a-2D-matrix/main.go
@@ -3,15 +3,42 @@ package main
 func searchMatrix(matrix [][]int, target int) bool {
 	height := len(matrix)
 	width := len(matrix[0])
-	for y := 0; y < height; y++ {
-		if matrix[y][0] == target || matrix[y][width-1] == target {
-			return true
-		}
-		if matrix[y][0] < target && matrix[y][width-1] > target {
-			return binarySearch(matrix[y], 0, width-1, target)
-		}
+
+	targetHeight, found := binarySearchForHeightIndex(matrix, 0, height-1, target)
+	if !found {
+		return false
 	}
-	return false
+	return binarySearch(matrix[targetHeight], 0, width-1, target)
+}
+
+func binarySearchForHeightIndex(matrix [][]int, min int, max int, target int) (int, bool) {
+	minValue := matrix[min][0]
+	maxValue := matrix[max][0]
+	if minValue == target {
+		return min, true
+	}
+	if maxValue == target {
+		return max, true
+	}
+	if minValue > target {
+		return min, false
+	}
+	// search last column
+	if maxValue < target {
+		return max, true
+	}
+	if max-min <= 1 {
+		return min, true
+	}
+	middle := (max + min) / 2
+	middleValue := matrix[middle][0]
+	if middleValue == target {
+		return middle, true
+	}
+	if middleValue > target {
+		return binarySearchForHeightIndex(matrix, min, middle-1, target)
+	}
+	return binarySearchForHeightIndex(matrix, middle, max, target)
 }
 
 func binarySearch(arr []int, left int, right int, target int) bool {
