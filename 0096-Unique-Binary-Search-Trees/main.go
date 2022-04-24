636c01070597df6e4301667b28ed78e99498ec19
daku10 <daku.two@gmail.com>
2022-04-24 20:15:39 +0900

    0096 improve performance

--- 0096-Unique-Binary-Search-Trees/main.go
+++ 0096-Unique-Binary-Search-Trees/main.go
@@ -5,9 +5,11 @@ func numTrees(n int) int {
 	dp[0] = 1
 	dp[1] = 1
 	for i := 2; i <= n; i++ {
-		for j := 0; j < i; j++ {
-			sum := i - 1
-			dp[i] += (dp[sum-j] * dp[j])
+		for j := 0; j < i/2; j++ {
+			dp[i] += (dp[i-1-j] * dp[j] * 2)
+		}
+		if i&1 == 1 {
+			dp[i] += dp[(i-1)/2] * dp[(i-1)/2]
 		}
 	}
 	return dp[n]
