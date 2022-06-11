a5406a3e09109dddcf1ef1b4cc990b15b32aa6df
daku10 <daku.two@gmail.com>
2022-06-12 00:41:19 +0900

    0097 use int for memo key

--- 0097-Interleaving-String/main.go
+++ 0097-Interleaving-String/main.go
@@ -1,14 +1,21 @@
 package main
 
-import "fmt"
-
 func isInterleave(s1 string, s2 string, s3 string) bool {
-	return isInterleaveSub(s1, s2, "", s3)
+	s1Len := len(s1)
+	s2Len := len(s2)
+	memo = make([][]int, s1Len+1)
+	for i := 0; i <= len(s1); i++ {
+		memo[i] = make([]int, s2Len+1)
+		for j := 0; j <= s2Len; j++ {
+			memo[i][j] = -1
+		}
+	}
+	return isInterleaveSub(s1, 0, s2, 0, "", s3)
 }
 
-var memo map[string]bool = make(map[string]bool)
+var memo [][]int
 
-func isInterleaveSub(s1 string, s2 string, result string, s3 string) bool {
+func isInterleaveSub(s1 string, s1Index int, s2 string, s2Index int, result string, s3 string) bool {
 	s1Len := len(s1)
 	s2Len := len(s2)
 	s3Len := len(s3)
@@ -21,27 +28,25 @@ func isInterleaveSub(s1 string, s2 string, result string, s3 string) bool {
 	if s2Len == 0 {
 		return s1 == s3
 	}
-	if v, ok := memo[fmt.Sprintf("%s_%s_%s", s1, s2, s3)]; ok {
-		if !v {
-			return false
-		}
+	if memo[s1Index][s2Index] == 0 {
+		return false
 	}
 	if s1[0] == s3[0] {
-		ans := isInterleaveSub(s1[1:s1Len], s2, result+string(s1[0]), s3[1:s3Len])
+		ans := isInterleaveSub(s1[1:s1Len], s1Index+1, s2, s2Index, result+string(s1[0]), s3[1:s3Len])
 		if ans {
 			return true
 		} else {
-			memo[fmt.Sprintf("%s_%s_%s", s1[1:s1Len], s2, s3[1:s3Len])] = false
+			memo[s1Index+1][s2Index] = 0
 		}
 	}
 	if s2[0] == s3[0] {
-		ans := isInterleaveSub(s1, s2[1:s2Len], result+string(s2[0]), s3[1:s3Len])
+		ans := isInterleaveSub(s1, s1Index, s2[1:s2Len], s2Index+1, result+string(s2[0]), s3[1:s3Len])
 		if ans {
 			return true
 		} else {
-			memo[fmt.Sprintf("%s_%s_%s", s1, s2[1:s2Len], s3[1:s3Len])] = false
+			memo[s1Index][s2Index+1] = 0
 		}
 	}
-	memo[fmt.Sprintf("%s_%s_%s", s1, s2, s3)] = false
+	memo[s1Index][s2Index] = 0
 	return false
 }
