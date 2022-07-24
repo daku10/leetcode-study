e17a11416dd1783fe7ac4e838f2d92aebc4a3825
daku10 <daku.two@gmail.com>
2022-07-24 18:29:47 +0900

    improve 0099 traverse once

--- 0099-Recover-Binary-Search-Tree/main.go
+++ 0099-Recover-Binary-Search-Tree/main.go
@@ -1,50 +1,49 @@
 package main
 
-import (
-	"sort"
-)
-
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
 	Right *TreeNode
 }
 
-func recoverTree(root *TreeNode) {
-	original := convertToInorder(root, nil)
-	sorted := make([]int, len(original))
-	copy(sorted, original)
-	sort.Ints(sorted)
-
-	invalid0 := -1
-	var invalid0Num, invalid1Num int
-
-	for i := 0; i < len(original); i++ {
-		if original[i] != sorted[i] {
-			if invalid0 == -1 {
-				invalid0 = i
-				invalid0Num = original[i]
-			} else {
-				invalid1Num = original[i]
-				break
-			}
-		}
-	}
-	traverse(root, invalid0Num, invalid1Num)
+type Object struct {
+	Pre       *TreeNode
+	Target    *TreeNode
+	Candidate *TreeNode
 }
 
-func traverse(node *TreeNode, invalid0Num int, invalid1Num int) {
-	if node == nil {
-		return
+func recoverTree(root *TreeNode) {
+	var isFirstFound bool = false
+	object := Object{}
+	traverse(root, &isFirstFound, &object)
+	if isFirstFound {
+		tmp := object.Candidate.Val
+		object.Candidate.Val = object.Target.Val
+		object.Target.Val = tmp
 	}
-	traverse(node.Left, invalid0Num, invalid1Num)
-	if node.Val == invalid0Num {
-		node.Val = invalid1Num
-	} else if node.Val == invalid1Num {
-		node.Val = invalid0Num
-		return
+}
+
+func traverse(node *TreeNode, isFirstFound *bool, object *Object) {
+	if node.Left != nil {
+		traverse(node.Left, isFirstFound, object)
 	}
-	traverse(node.Right, invalid0Num, invalid1Num)
+	if object.Pre != nil && object.Pre.Val > node.Val {
+		if !*isFirstFound {
+			*isFirstFound = true
+			object.Target = object.Pre
+			object.Candidate = node
+		} else {
+			tmp := node.Val
+			node.Val = object.Target.Val
+			object.Target.Val = tmp
+			*isFirstFound = false
+			return
+		}
+	}
+	object.Pre = node
+	if node.Right != nil {
+		traverse(node.Right, isFirstFound, object)
+	}
 }
 
 func convertToInorder(root *TreeNode, arg []int) []int {
--- 0099-Recover-Binary-Search-Tree/main_test.go
+++ 0099-Recover-Binary-Search-Tree/main_test.go
@@ -32,13 +32,39 @@ func TestRecoverBinarySearchTree(t *testing.T) {
 				},
 			},
 		},
+		{
+			arg: &TreeNode{
+				Val: 3,
+				Left: &TreeNode{
+					Val: 1,
+				},
+				Right: &TreeNode{
+					Val: 4,
+					Left: &TreeNode{
+						Val: 2,
+					},
+				},
+			},
+			want: &TreeNode{
+				Val: 2,
+				Left: &TreeNode{
+					Val: 1,
+				},
+				Right: &TreeNode{
+					Val: 4,
+					Left: &TreeNode{
+						Val: 3,
+					},
+				},
+			},
+		},
 	}
 
 	for i, tc := range testcases {
 		t.Run(fmt.Sprint(i), func(t *testing.T) {
 			recoverTree(tc.arg)
 			if !reflect.DeepEqual(convertToInorder(tc.want, nil), convertToInorder(tc.arg, nil)) {
-				t.Errorf("failed want: %v arg: %v", tc.want, tc.arg)
+				t.Errorf("failed want: %v arg: %v", convertToInorder(tc.want, nil), convertToInorder(tc.arg, nil))
 			}
 		})
 	}
