febade601594031233a3a5e1dd03d1cbfc995cff
daku10 <daku.two@gmail.com>
2022-08-27 15:49:43 +0900

    116 not to use bfs

--- 116-Populating-Next-Right-Pointers-in-Each-Node/main.go
+++ 116-Populating-Next-Right-Pointers-in-Each-Node/main.go
@@ -11,25 +11,16 @@ func connect(root *Node) *Node {
 	if root == nil {
 		return nil
 	}
-	queue := []*Node{root}
-	var nextQueue []*Node
-	for len(queue) != 0 {
-		var tmp *Node
-		for _, n := range queue {
-			if tmp != nil {
-				n.Next = tmp
-			}
-			tmp = n
-			if n.Right != nil {
-				nextQueue = append(nextQueue, n.Right)
-			}
-			if n.Left != nil {
-				nextQueue = append(nextQueue, n.Left)
+	if root.Left != nil {
+		root.Left.Next = root.Right
+		if root.Right != nil {
+			if root.Next != nil {
+				root.Right.Next = root.Next.Left
 			}
 		}
-		queue = nextQueue
-		nextQueue = nil
 	}
+	connect(root.Left)
+	connect(root.Right)
 
 	return root
 }
