5b68ec04556986bb633660442b6d91367c38d4ff
daku10 <daku.two@gmail.com>
2022-09-04 20:41:24 +0900

    solve 117

--- 117-Populating-Next-Right-Pointers-in-Each-Node-II/main.go
+++ 117-Populating-Next-Right-Pointers-in-Each-Node-II/main.go
@@ -11,39 +11,22 @@ func connect(root *Node) *Node {
 	if root == nil {
 		return nil
 	}
-	if root.Left != nil {
-		if root.Right != nil {
-			root.Left.Next = root.Right
-		} else {
-			next := root.Next
-			for next != nil {
-				if next.Left != nil {
-					root.Left.Next = next.Left
-					break
-				}
-				if next.Right != nil {
-					root.Left.Next = next.Right
-					break
-				}
-				next = next.Next
+	queue := []*Node{root}
+	var nextQueue []*Node
+	for len(queue) != 0 {
+		var nextN *Node
+		for _, n := range queue {
+			if n.Right != nil {
+				nextQueue = append(nextQueue, n.Right)
 			}
+			if n.Left != nil {
+				nextQueue = append(nextQueue, n.Left)
+			}
+			n.Next = nextN
+			nextN = n
 		}
+		queue = nextQueue
+		nextQueue = nil
 	}
-	if root.Right != nil {
-		next := root.Next
-		for next != nil {
-			if next.Left != nil {
-				root.Right.Next = next.Left
-				break
-			}
-			if next.Right != nil {
-				root.Right.Next = next.Right
-				break
-			}
-			next = next.Next
-		}
-	}
-	connect(root.Left)
-	connect(root.Right)
 	return root
 }
