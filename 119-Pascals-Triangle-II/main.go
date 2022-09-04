f85524392d0eb2f3fc27b99afc9220b848bda58b
daku10 <daku.two@gmail.com>
2022-09-04 23:22:24 +0900

    119 use only one slice to memorize

--- 119-Pascals-Triangle-II/main.go
+++ 119-Pascals-Triangle-II/main.go
@@ -1,15 +1,16 @@
 package main
 
 func getRow(rowIndex int) []int {
-	var result [][]int
+	var prev []int
+	var res []int
 	for i := 0; i <= rowIndex; i++ {
-		res := make([]int, i+1)
+		res = make([]int, i+1)
 		res[0] = 1
 		res[i] = 1
 		for j := 1; j < i; j++ {
-			res[j] = result[i-1][j-1] + result[i-1][j]
+			res[j] = prev[j-1] + prev[j]
 		}
-		result = append(result, res)
+		prev = res
 	}
-	return result[rowIndex]
+	return res
 }
