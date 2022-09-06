d7f190b51233815ef7fefb7da180703341c1d9da
daku10 <daku.two@gmail.com>
2022-09-06 23:58:13 +0900

    solve 121 but slowly

--- 121-Best-Time-to-Buy-and-Sell-Stock/main.go
+++ 121-Best-Time-to-Buy-and-Sell-Stock/main.go
@@ -1,8 +1,15 @@
 package main
 
+import "math"
+
 func maxProfit(prices []int) int {
 	result := 0
+	cheapestPrice := math.MaxInt
 	for i := 0; i < len(prices); i++ {
+		if prices[i] >= cheapestPrice {
+			continue
+		}
+		cheapestPrice = prices[i]
 		for j := i + 1; j < len(prices); j++ {
 			res := prices[j] - prices[i]
 			if res > result {
