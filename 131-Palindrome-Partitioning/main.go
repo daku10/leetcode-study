b795e1c2d16201df64d1d7e607bf8d8653f48c03
daku10 <daku.two@gmail.com>
2023-02-28 20:11:29 +0900

    solve 131 but too ugly

--- 131-Palindrome-Partitioning/main.go
+++ 131-Palindrome-Partitioning/main.go
@@ -1,19 +1,11 @@
 package main
 
-func partition(s string) [][]string {
-	var result [][]string
-	candidates := createPartition(s)
-L:
-	for _, c := range candidates {
-		for _, r := range result {
-			if isEqual(c, r) {
-				continue L
-			}
-		}
-		result = append(result, c)
-	}
+import "strings"
 
-	return result
+func partition(s string) [][]string {
+	memo := make(map[string][][]string)
+	memo2 := make(map[string]struct{})
+	return createPartition(s, memo, memo2)
 }
 
 func isEqual(a []string, b []string) bool {
@@ -28,27 +20,45 @@ func isEqual(a []string, b []string) bool {
 	return true
 }
 
-func createPartition(s string) [][]string {
+func createPartition(s string, memo map[string][][]string, memo2 map[string]struct{}) [][]string {
 	if len(s) == 1 {
 		return [][]string{{s}}
+	}
+	if len(s) == 2 {
+		if isPalindrome(s) {
+			return [][]string{{s}, {s[:1], s[1:]}}
+		}
+		return [][]string{{s[:1], s[1:]}}
+	}
+	if v, ok := memo[s]; ok {
+		return v
 	}
 	result := [][]string{}
 	if isPalindrome(s) {
 		result = append(result, []string{s})
 	}
 	for i := 1; i < len(s); i++ {
-		before := createPartition(s[:i])
-		after := createPartition(s[i:])
+		before := createPartition(s[:i], memo, memo2)
+		after := createPartition(s[i:], memo, memo2)
 		for _, b := range before {
 			for _, a := range after {
 				n := make([]string, len(b)+len(a))
 				copy(n, b)
 				copy(n[len(b):], a)
-				result = append(result, n)
+				k := toKey(n)
+				if _, ok := memo2[k]; !ok {
+					result = append(result, n)
+					memo2[k] = struct{}{}
+				}
 			}
 		}
 	}
+	memo[s] = result
 	return result
+}
+
+func toKey(arg []string) string {
+	return strings.Join(arg, "_")
 }
 
 func isPalindrome(s string) bool {
@@ -60,24 +70,3 @@ func isPalindrome(s string) bool {
 	}
 	return true
 }
-
-// "aab"
-// -> {{"aa", "b"}, {"a", "a", "b"}}
-// "aa"
-// -> {{"aa"}, {"a", "a"}}
-// "aaa"
-// -> {{"aaa"}, {"aa", "a"}, {"a", "a", "a"}}
-// 数学的に組み合わせを作ってpalindromeだけ入れるができそう？
-
-// func createPartition(s string, result *[]string) {
-// 	if len(s) == 1 {
-// 		return
-// 	}
-// 	if isPalindrome(s) {
-// 		*result = append(*result, s)
-// 	}
-// 	for i := 1; i < len(s); i++ {
-// 		createPartition(s[:i], result)
-// 		createPartition(s[i:], result)
-// 	}
-// }
