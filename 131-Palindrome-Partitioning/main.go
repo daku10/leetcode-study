8fe78da91e34df85bda9ac79996553f61723dbf7
daku10 <daku.two@gmail.com>
2023-03-04 19:56:10 +0900

    improve 131

--- 131-Palindrome-Partitioning/main.go
+++ 131-Palindrome-Partitioning/main.go
@@ -1,64 +1,25 @@
 package main
 
-import "strings"
-
 func partition(s string) [][]string {
-	memo := make(map[string][][]string)
-	memo2 := make(map[string]struct{})
-	return createPartition(s, memo, memo2)
-}
-
-func isEqual(a []string, b []string) bool {
-	if len(a) != len(b) {
-		return false
-	}
-	for i := 0; i < len(a); i++ {
-		if a[i] != b[i] {
-			return false
-		}
-	}
-	return true
-}
-
-func createPartition(s string, memo map[string][][]string, memo2 map[string]struct{}) [][]string {
-	if len(s) == 1 {
-		return [][]string{{s}}
-	}
-	if len(s) == 2 {
-		if isPalindrome(s) {
-			return [][]string{{s}, {s[:1], s[1:]}}
-		}
-		return [][]string{{s[:1], s[1:]}}
-	}
-	if v, ok := memo[s]; ok {
-		return v
-	}
 	result := [][]string{}
-	if isPalindrome(s) {
-		result = append(result, []string{s})
-	}
-	for i := 1; i < len(s); i++ {
-		before := createPartition(s[:i], memo, memo2)
-		after := createPartition(s[i:], memo, memo2)
-		for _, b := range before {
-			for _, a := range after {
-				n := make([]string, len(b)+len(a))
-				copy(n, b)
-				copy(n[len(b):], a)
-				k := toKey(n)
-				if _, ok := memo2[k]; !ok {
-					result = append(result, n)
-					memo2[k] = struct{}{}
-				}
-			}
-		}
-	}
-	memo[s] = result
+	current := []string{}
+	dfs(0, &result, &current, s)
 	return result
 }
 
-func toKey(arg []string) string {
-	return strings.Join(arg, "_")
+func dfs(start int, result *[][]string, current *[]string, s string) {
+	if start >= len(s) {
+		res := make([]string, len(*current))
+		copy(res, *current)
+		*result = append(*result, res)
+	}
+	for end := start; end < len(s); end++ {
+		if isPalindrome(s[start : end+1]) {
+			*current = append(*current, s[start:end+1])
+			dfs(end+1, result, current, s)
+			*current = ((*current)[:len(*current)-1])
+		}
+	}
 }
 
 func isPalindrome(s string) bool {
