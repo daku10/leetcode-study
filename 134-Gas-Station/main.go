daab857b793e62e942a8bd55ecb95ba2c13035e6
daku10 <daku.two@gmail.com>
2023-03-05 16:18:50 +0900

    134 improve

--- 134-Gas-Station/main.go
+++ 134-Gas-Station/main.go
@@ -1,54 +1,20 @@
 package main
 
 func canCompleteCircuit(gas []int, cost []int) int {
-	candidates, exists := findCandidates(gas, cost)
-	if len(gas) == 1 {
-		if gas[0] >= cost[0] {
-			return 0
-		}
-		return -1
-	}
-	if !exists {
-		return -1
-	}
-	for _, c := range candidates {
-		if canTravel(gas, cost, c) {
-			return c
-		}
-	}
-	return -1
-}
-
-func findCandidates(gas []int, cost []int) ([]int, bool) {
-	var candidates []int
 	sum := 0
-	for i := 0; i < len(gas); i++ {
-		ci := gas[i] - cost[i]
-		sum += ci
-		if ci < 0 {
-			j := ((i + 1) % len(gas))
-			if gas[j]-cost[j] > 0 {
-				candidates = append(candidates, j)
-			}
+	l := len(gas)
+	startPoint := 0
+	currentGas := 0
+	for i := 0; i < l; i++ {
+		sum += (gas[i] - cost[i])
+		currentGas += (gas[i] - cost[i])
+		if currentGas < 0 {
+			startPoint = i + 1
+			currentGas = 0
 		}
 	}
 	if sum < 0 {
-		return nil, false
+		return -1
 	}
-	return candidates, true
-}
-
-func canTravel(gas []int, cost []int, start int) bool {
-	tank := gas[start]
-	end := ((start - 1) + len(gas)) % len(gas)
-	for s := start; s != end; {
-		tank -= cost[s]
-		if tank < 0 {
-			return false
-		}
-		s++
-		s %= len(gas)
-		tank += gas[s]
-	}
-	return tank >= cost[end]
+	return startPoint
 }
