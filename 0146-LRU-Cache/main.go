f2a4fc67e0e940325e74f4ceee13f7504ab89abc
daku10 <daku.two@gmail.com>
2023-03-19 22:32:28 +0900

    0146 improve performance to use LinkedList

--- 0146-LRU-Cache/main.go
+++ 0146-LRU-Cache/main.go
@@ -2,71 +2,99 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
+type Node struct {
+	Prev *Node
+	Next *Node
+	Key  int
+}
+
 type LRUCache struct {
-	kv map[int]struct {
-		val int
-		age int
-	}
-	cap        int
-	currentAge int
+	kv   map[int]int
+	age  *Node
+	kAge map[int]*Node
+	cap  int
 }
 
 func Constructor(capacity int) LRUCache {
+	n := &Node{Key: -1}
+	n.Next = n
+	n.Prev = n
 	return LRUCache{
-		kv: make(map[int]struct {
-			val int
-			age int
-		}),
-		cap:        capacity,
-		currentAge: 0,
+		kv:   make(map[int]int),
+		cap:  capacity,
+		age:  n, // dummy
+		kAge: make(map[int]*Node),
 	}
 }
 
 func (this *LRUCache) Get(key int) int {
-	this.currentAge++
 	if v, ok := this.kv[key]; ok {
-		this.kv[key] = struct {
-			val int
-			age int
-		}{
-			val: v.val,
-			age: this.currentAge,
-		}
-		return v.val
+		n := this.kAge[key]
+		n.Prev.Next = n.Next
+		n.Next.Prev = n.Prev
+		n.Next = this.age.Next
+		n.Prev = this.age
+		this.age.Next.Prev = n
+		this.age.Next = n
+		return v
 	}
 	return -1
 }
 
 func (this *LRUCache) Put(key int, value int) {
-	this.currentAge++
-	this.kv[key] = struct {
-		val int
-		age int
-	}{
-		val: value,
-		age: this.currentAge,
-	}
-	if len(this.kv) > this.cap {
-		min := math.MaxInt
-		minK := 0
-		for k, v := range this.kv {
-			if min > v.age {
-				min = v.age
-				minK = k
-			}
+	if n, ok := this.kAge[key]; ok {
+		n.Next.Prev = n.Prev
+		n.Prev.Next = n.Next
+		n.Next = this.age.Next
+		n.Prev = this.age
+		this.age.Next.Prev = n
+		this.age.Next = n
+	} else {
+		node := &Node{
+			Key: key,
 		}
-		delete(this.kv, minK)
+		this.kAge[key] = node
+		node.Next = this.age.Next
+		node.Prev = this.age
+		this.age.Next.Prev = node
+		this.age.Next = node
+	}
+	this.kv[key] = value
+	if len(this.kv) > this.cap {
+		n := this.age.Prev
+		k := n.Key
+		delete(this.kv, k)
+		delete(this.kAge, k)
+		n.Prev.Next = this.age
+		this.age.Prev = n.Prev
+		n.Prev = nil
+		n.Next = nil
 	}
 }
 
 func (this *LRUCache) Debug() {
 	fmt.Println("kv")
 	fmt.Println(this.kv)
-	fmt.Println("currentAge")
-	fmt.Println(this.currentAge)
 	fmt.Println("cap")
 	fmt.Println(this.cap)
+	fmt.Println("age next")
+	n := this.age
+	for {
+		fmt.Println(n.Key)
+		n = n.Next
+		if n == this.age {
+			break
+		}
+	}
+	fmt.Println("age prev")
+	n = this.age
+	for {
+		fmt.Println(n.Key)
+		n = n.Prev
+		if n == this.age {
+			break
+		}
+	}
 }
--- 0146-LRU-Cache/main_test.go
+++ 0146-LRU-Cache/main_test.go
@@ -1,9 +1,11 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestLRUCache(t *testing.T) {
-	t.Run("LRU Cache", func(t *testing.T) {
+	t.Run("LRU Cache 1", func(t *testing.T) {
 		cache := Constructor(2)
 		cache.Put(1, 1)
 		cache.Put(2, 2)
@@ -30,4 +32,114 @@ func TestLRUCache(t *testing.T) {
 			t.Errorf("actual: %v want: %v", v, 4)
 		}
 	})
+
+	t.Run("LRU Cache 2", func(t *testing.T) {
+		cache := Constructor(2)
+		cache.Get(2)
+		cache.Put(2, 6)
+		cache.Get(1)
+		cache.Put(1, 5)
+		cache.Put(1, 2)
+		if v := cache.Get(1); v != 2 {
+			t.Fatalf("actual: %v want: %v", v, 2)
+		}
+		if v := cache.Get(2); v != 6 {
+			t.Errorf("actual: %v want: %v", v, 6)
+		}
+		// cache.Put(1, 1)
+		// cache.Put(2, 2)
+		// v := cache.Get(1)
+		// if v != 1 {
+		// 	t.Fatalf("actual: %v want: %v", v, 1)
+		// }
+		// cache.Put(3, 3)
+		// v = cache.Get(2)
+		// if v != -1 {
+		// 	t.Fatalf("actual: %v want: %v", v, -1)
+		// }
+		// cache.Put(4, 4)
+		// v = cache.Get(1)
+		// if v != -1 {
+		// 	t.Fatalf("actual: %v want: %v", v, -1)
+		// }
+		// v = cache.Get(3)
+		// if v != 3 {
+		// 	t.Fatalf("actual: %v want: %v", v, 3)
+		// }
+		// v = cache.Get(4)
+		// if v != 4 {
+		// 	t.Errorf("actual: %v want: %v", v, 4)
+		// }
+	})
+
+	t.Run("LRU Cache 3", func(t *testing.T) {
+		// 		["LRUCache","put","put","put","put","put","get","put","get","get","put","get","put","put","put","get","put","get","get","get","get","put","put","get","get","get","put","put","get","put","get","put","get","get","get","put","put","put","get","put","get","get","put","put","get","put","put","put","put","get","put","put","get","put","put","get","put","put","put","put","put","get","put","put","get","put","get","get","get","put","get","get","put","put","put","put","get","put","put","put","put","get","get","get","put","put","put","get","put","put","put","get","put","put","put","get","get","get","put","put","put","put","get","put","put","put","put","put","put","put"]
+		// [[10],[10,13],[3,17],[6,11],[10,5],[9,10],[13],[2,19],[2],[3],[5,25],[8],[9,22],[5,5],[1,30],[11],[9,12],[7],[5],[8],[9],[4,30],[9,3],[9],[10],[10],[6,14],[3,1],[3],[10,11],[8],[2,14],[1],[5],[4],[11,4],[12,24],[5,18],[13],[7,23],[8],[12],[3,27],[2,12],[5],[2,9],[13,4],[8,18],[1,7],[6],[9,29],[8,21],[5],[6,30],[1,12],[10],[4,15],[7,22],[11,26],[8,17],[9,29],[5],[3,4],[11,30],[12],[4,29],[3],[9],[6],[3,4],[1],[10],[3,29],[10,28],[1,20],[11,13],[3],[3,12],[3,8],[10,9],[3,26],[8],[7],[5],[13,17],[2,27],[11,15],[12],[9,19],[2,15],[3,16],[1],[12,17],[9,1],[6,19],[4],[5],[5],[8,1],[11,7],[5,2],[9,28],[1],[2,2],[7,4],[4,22],[7,24],[9,26],[13,28],[11,26]]
+		cache := Constructor(10)
+		cache.Put(10, 13)
+		cache.Put(3, 17)
+		cache.Put(6, 11)
+		cache.Put(10, 5)
+
+		cache.Put(9, 10)
+		cache.Get(13)
+		cache.Put(2, 19)
+		cache.Get(2)
+		cache.Get(3)
+		cache.Put(5, 25)
+		cache.Get(8)
+		cache.Put(9, 22)
+		cache.Put(5, 5)
+		cache.Put(1, 30)
+		cache.Get(11)
+		cache.Put(9, 12)
+		cache.Get(7)
+		cache.Get(5)
+		cache.Get(8)
+		cache.Get(9)
+		cache.Put(4, 30)
+		cache.Put(9, 3)
+		cache.Get(9)
+		cache.Get(10)
+		cache.Get(10)
+		cache.Put(6, 14)
+		cache.Put(3, 1)
+		cache.Get(3)
+		cache.Put(10, 11)
+		cache.Get(8)
+		cache.Put(2, 14)
+		cache.Get(1)
+		cache.Get(5)
+		cache.Get(4)
+		cache.Put(11, 4)
+		cache.Put(12, 24)
+		cache.Put(5, 18)
+		cache.Get(13)
+		cache.Put(7, 23)
+		cache.Get(8)
+		cache.Get(12)
+		cache.Put(3, 27)
+		cache.Put(2, 12)
+		cache.Get(5)
+		cache.Put(2, 9)
+		cache.Put(13, 4)
+		cache.Put(8, 18)
+		cache.Put(1, 7)
+		cache.Get(6)
+		cache.Put(9, 29)
+		cache.Put(8, 21)
+		cache.Get(5)
+		cache.Put(6, 30)
+		cache.Put(1, 12)
+		cache.Get(10)
+		cache.Put(4, 15)
+		cache.Put(7, 22)
+		cache.Put(11, 26)
+		cache.Put(8, 17)
+		cache.Put(9, 29)
+		cache.Get(5)
+		cache.Put(3, 4)
+		cache.Put(11, 30)
+	})
+
 }
