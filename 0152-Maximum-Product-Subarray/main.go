7fe0c01eeb968398db221257e51d698c338c8aaf
daku10 <daku.two@gmail.com>
2023-03-24 20:52:09 +0900

    0152 solve

--- 0152-Maximum-Product-Subarray/main.go
+++ 0152-Maximum-Product-Subarray/main.go
@@ -1,23 +1,19 @@
 package main
 
-import "math"
+import (
+	"math"
+)
 
 func maxProduct(nums []int) int {
 	result := math.MinInt
 	for i := 0; i < len(nums); i++ {
-		for j := i + 1; j <= len(nums); j++ {
-			if v := product(nums[i:j]); v > result {
-				result = v
+		current := 1
+		for j := i; j < len(nums); j++ {
+			current *= nums[j]
+			if result < current {
+				result = current
 			}
 		}
-	}
-	return result
-}
-
-func product(nums []int) int {
-	result := 1
-	for _, num := range nums {
-		result *= num
 	}
 	return result
 }
