fb24a57885491505873b79d8706d7dd38cb70be2
daku10 <daku.two@gmail.com>
2023-03-24 21:31:49 +0900

    0152 improve

--- 0152-Maximum-Product-Subarray/main.go
+++ 0152-Maximum-Product-Subarray/main.go
@@ -6,13 +6,28 @@ import (
 
 func maxProduct(nums []int) int {
 	result := math.MinInt
+
+	// nums contains even negative numbers, this indicates result
+	current := 1
 	for i := 0; i < len(nums); i++ {
-		current := 1
-		for j := i; j < len(nums); j++ {
-			current *= nums[j]
-			if result < current {
-				result = current
-			}
+		current *= nums[i]
+		if current > result {
+			result = current
+		}
+		if current == 0 {
+			current = 1
+		}
+	}
+
+	// nums contains odd negative numbers, this indicates result
+	current = 1
+	for i := len(nums) - 1; i >= 0; i-- {
+		current *= nums[i]
+		if current > result {
+			result = current
+		}
+		if current == 0 {
+			current = 1
 		}
 	}
 	return result
