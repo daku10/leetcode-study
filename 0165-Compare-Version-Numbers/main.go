82728c1fdfa783c6f5c71820d4a9ee18498a2a00
daku10 <daku.two@gmail.com>
2023-03-27 19:15:33 +0900

    0165 improve not to allocate additional slice

--- 0165-Compare-Version-Numbers/main.go
+++ 0165-Compare-Version-Numbers/main.go
@@ -19,23 +19,32 @@ func compareVersion(version1 string, version2 string) int {
 		v2Nums[i] = a
 	}
 
+	maxLen := len(v1Nums)
 	if len(v1Nums) > len(v2Nums) {
 		diff := len(v1Nums) - len(v2Nums)
 		for i := 0; i < diff; i++ {
 			v2Nums = append(v2Nums, 0)
 		}
 	} else if len(v1Nums) < len(v2Nums) {
+		maxLen = len(v2Nums)
 		diff := len(v2Nums) - len(v1Nums)
 		for i := 0; i < diff; i++ {
 			v1Nums = append(v1Nums, 0)
 		}
 	}
 
-	for i := range v1Nums {
-		if v1Nums[i] > v2Nums[i] {
+	for i := 0; i < maxLen; i++ {
+		var v1, v2 int
+		if i < len(v1Nums) {
+			v1 = v1Nums[i]
+		}
+		if i < len(v2Nums) {
+			v2 = v2Nums[i]
+		}
+		if v1 > v2 {
 			return 1
 		}
-		if v2Nums[i] > v1Nums[i] {
+		if v2 > v1 {
 			return -1
 		}
 	}
