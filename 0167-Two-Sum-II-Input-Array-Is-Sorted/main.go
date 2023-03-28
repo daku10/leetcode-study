c2932ee5c73d50291118cdcdc25ad250a8adfd90
daku10 <daku.two@gmail.com>
2023-03-28 18:00:10 +0900

    0167 make faster to use binary search

--- 0167-Two-Sum-II-Input-Array-Is-Sorted/main.go
+++ 0167-Two-Sum-II-Input-Array-Is-Sorted/main.go
@@ -3,7 +3,7 @@ package main
 func twoSum(numbers []int, target int) []int {
 	for i, n := range numbers {
 		s := target - n
-		res := search(numbers, s, i)
+		res := binarySearch(numbers, s, i, len(numbers)-1, i)
 		if res != -1 {
 			return []int{i + 1, res + 1}
 		}
@@ -11,17 +11,34 @@ func twoSum(numbers []int, target int) []int {
 	return nil
 }
 
-func search(nums []int, s int, ignoreIndex int) int {
-	for i, n := range nums {
-		if i == ignoreIndex {
-			continue
+func binarySearch(nums []int, target int, start int, end int, ignore int) int {
+	if start == end {
+		if nums[start] == target && start != ignore {
+			return start
 		}
-		if n == s {
-			return i
+		return -1
+	}
+	if end-start == 1 {
+		if nums[start] == target && start != ignore {
+			return start
 		}
-		if n > s {
-			return -1
+		if nums[end] == target && end != ignore {
+			return end
+		}
+		return -1
+	}
+	mid := (start + end) / 2
+	numMid := nums[mid]
+	if numMid == target {
+		if mid != ignore {
+			return mid
+		}
+		if nums[mid+1] == ignore {
+			return mid + 1
 		}
 	}
-	return -1
+	if numMid < target {
+		return binarySearch(nums, target, mid, end, ignore)
+	}
+	return binarySearch(nums, target, start, mid, ignore)
 }
