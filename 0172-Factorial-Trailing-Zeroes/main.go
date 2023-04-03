7e71ccb52107e7384edb1719852278fd73e4b3ce
daku10 <daku.two@gmail.com>
2023-04-03 18:37:56 +0900

    0172 improve to use O(log5n)

--- 0172-Factorial-Trailing-Zeroes/main.go
+++ 0172-Factorial-Trailing-Zeroes/main.go
@@ -4,33 +4,13 @@ func trailingZeroes(n int) int {
 	if n == 0 {
 		return 0
 	}
+
 	result := 0
-	twoCount := 0
-	fiveCount := 0
-	for i := 1; i <= n; i++ {
-		j := i
-		for {
-			if j%2 == 0 {
-				twoCount++
-				j /= 2
-			} else {
-				break
-			}
-		}
-		j = i
-		for {
-			if j%5 == 0 {
-				fiveCount++
-				j /= 5
-			} else {
-				break
-			}
-		}
-		for twoCount > 0 && fiveCount > 0 {
-			result++
-			twoCount--
-			fiveCount--
-		}
+	for n > 0 {
+		tmp := n / 5
+		result += tmp
+		n = tmp
 	}
+
 	return result
 }
