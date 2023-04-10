4048a8ad6898758bf3800dc7d9a2f17c00d40656
daku10 <daku.two@gmail.com>
2023-04-10 21:46:10 +0900

    0187 solve

--- 0187-Repeated-DNA-Sequences/main.go
+++ 0187-Repeated-DNA-Sequences/main.go
@@ -6,17 +6,17 @@ func findRepeatedDnaSequences(s string) []string {
 	}
 	var result []string
 	memo := make(map[string]struct{})
+	resultMemo := make(map[string]struct{})
 	for i := 0; i <= len(s)-10; i++ {
 		target := s[i : i+10]
 		if _, ok := memo[target]; ok {
-			continue
-		}
-		for j := i + 1; j <= len(s)-10; j++ {
-			if target == s[j:j+10] {
-				result = append(result, target)
-				memo[target] = struct{}{}
-				break
+			if _, ok := resultMemo[target]; ok {
+				continue
 			}
+			result = append(result, target)
+			resultMemo[target] = struct{}{}
+		} else {
+			memo[target] = struct{}{}
 		}
 	}
 	return result
