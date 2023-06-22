3c454851451fe786240adc4316d177953e45dbdf
daku10 <daku10.dev@gmail.com>
2023-06-22 21:16:28 +0900

    0204 skip even number to improve

--- 0204-Count-Primes/main.go
+++ 0204-Count-Primes/main.go
@@ -4,21 +4,20 @@ func countPrimes(n int) int {
 	if n < 3 {
 		return 0
 	}
-	notPrimeMap := make([]int, n)
-	count := 0
-	for i := 2; i < n; i++ {
-		if notPrimeMap[i] == 1 {
-			continue
-		}
-		count++
-		j := 2
-		for {
-			o := i * j
-			if o >= n {
-				break
+	notPrimes := make([]bool, n)
+	count := 1
+	for i := 3; i < n; i += 2 {
+		if !notPrimes[i] {
+			count++
+			j := 3
+			for {
+				o := i * j
+				if o >= n {
+					break
+				}
+				notPrimes[o] = true
+				j += 2
 			}
-			notPrimeMap[o] = 1
-			j++
 		}
 	}
 	return count
