e62008ff2bd4f6c1a042377ce64c9a8ead4008f5
daku10 <daku10.dev@gmail.com>
2023-07-08 23:04:15 +0900

    0208 improve performance to use fixed slice instead of map

--- 0208-Implement-Trie-Prefix-Tree/main.go
+++ 0208-Implement-Trie-Prefix-Tree/main.go
@@ -1,36 +1,34 @@
 package main
 
 type Node struct {
-	Val  byte
-	Next map[byte]*Node
+	Next []*Node
 }
 
 func NewNode(v byte) *Node {
 	return &Node{
-		Val:  v,
-		Next: make(map[byte]*Node),
+		Next: make([]*Node, 26),
 	}
 }
 
 type Trie struct {
 	dict  map[string]struct{}
-	Nodes map[byte]*Node
+	Nodes []*Node
 }
 
 func Constructor() Trie {
 	return Trie{
 		make(map[string]struct{}),
-		make(map[byte]*Node),
+		make([]*Node, 26),
 	}
 }
 
 func (this *Trie) Insert(word string) {
 	this.dict[word] = struct{}{}
 	p := word[0]
-	node, ok := this.Nodes[p]
-	if !ok {
+	node := this.Nodes[p-97]
+	if node == nil {
 		node = NewNode(p)
-		this.Nodes[p] = node
+		this.Nodes[p-97] = node
 	}
 	if len(word) == 1 {
 		return
@@ -40,10 +38,10 @@ func (this *Trie) Insert(word string) {
 
 func (n *Node) Insert(word string) {
 	p := word[0]
-	node, ok := n.Next[p]
-	if !ok {
+	node := n.Next[p-97]
+	if node == nil {
 		node = NewNode(p)
-		n.Next[p] = node
+		n.Next[p-97] = node
 	}
 	if len(word) == 1 {
 		return
@@ -60,8 +58,8 @@ func (this *Trie) Search(word string) bool {
 
 func (this *Trie) StartsWith(prefix string) bool {
 	p := prefix[0]
-	n, ok := this.Nodes[p]
-	if !ok {
+	n := this.Nodes[p-97]
+	if n == nil {
 		return false
 	}
 	if len(prefix) == 1 {
@@ -72,8 +70,8 @@ func (this *Trie) StartsWith(prefix string) bool {
 
 func (n *Node) StartsWith(prefix string) bool {
 	p := prefix[0]
-	next, ok := n.Next[p]
-	if !ok {
+	next := n.Next[p-97]
+	if next == nil {
 		return false
 	}
 	if len(prefix) == 1 {
