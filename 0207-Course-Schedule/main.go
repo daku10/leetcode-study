9a5edbc412a4638df6f9f962c9d4dd8ee6c9d9dd
daku10 <daku10.dev@gmail.com>
2023-07-08 19:43:04 +0900

    0207 improve to use pseudo topological sorting

--- 0207-Course-Schedule/main.go
+++ 0207-Course-Schedule/main.go
@@ -1,71 +1,32 @@
 package main
 
-type Node struct {
-	Val  int
-	Next []*Node
-}
-
 func canFinish(numCourses int, prerequisites [][]int) bool {
-	if len(prerequisites) == 0 {
-		return true
-	}
-
-	memo := make(map[int]*Node)
-	heads := make(map[*Node]struct{})
-
+	inDegree := make([]int, numCourses)
+	adj := make([][]int, numCourses)
 	for _, p := range prerequisites {
-		after := p[0]
-		before := p[1]
-		var afterNode, beforeNode *Node
-		if v, ok := memo[after]; ok {
-			afterNode = v
-		} else {
-			afterNode = &Node{
-				Val: after,
+		adj[p[1]] = append(adj[p[1]], p[0])
+		inDegree[p[0]]++
+	}
+
+	var candidates []int
+	for index, deg := range inDegree {
+		if deg == 0 {
+			candidates = append(candidates, index)
+		}
+	}
+	numVisited := 0
+	for len(candidates) != 0 {
+		// take last elemenet from candidates, therefore this is not topological sort
+		node := candidates[len(candidates)-1]
+		candidates = candidates[:len(candidates)-1]
+		numVisited++
+		ad := adj[node]
+		for _, a := range ad {
+			inDegree[a]--
+			if inDegree[a] == 0 {
+				candidates = append(candidates, a)
 			}
-			memo[after] = afterNode
-		}
-		if v, ok := memo[before]; ok {
-			beforeNode = v
-		} else {
-			beforeNode = &Node{
-				Val: before,
-			}
-			memo[before] = beforeNode
-		}
-		beforeNode.Next = append(beforeNode.Next, afterNode)
-		delete(heads, afterNode)
-		heads[beforeNode] = struct{}{}
-	}
-
-	for k := range heads {
-		visited := make(map[*Node]struct{})
-		okNodes := make(map[*Node]struct{})
-		if hasCycle(k, visited, okNodes) {
-			return false
 		}
 	}
-
-	return true
-}
-
-func hasCycle(current *Node, visited map[*Node]struct{}, okNodes map[*Node]struct{}) bool {
-	if _, ok := visited[current]; ok {
-		return true
-	}
-	if _, ok := okNodes[current]; ok {
-		return false
-	}
-	if current.Next == nil {
-		return false
-	}
-	visited[current] = struct{}{}
-	for _, n := range current.Next {
-		if hasCycle(n, visited, okNodes) {
-			return true
-		}
-	}
-	delete(visited, current)
-	okNodes[current] = struct{}{}
-	return false
+	return numVisited == numCourses
 }
