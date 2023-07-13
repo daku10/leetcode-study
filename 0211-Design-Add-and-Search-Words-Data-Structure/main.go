529553909a78acd2da25872def5fe2cf7731a878
daku10 <daku10.dev@gmail.com>
2023-07-13 21:59:31 +0900

    0211 improve to use Trie

--- 0211-Design-Add-and-Search-Words-Data-Structure/main.go
+++ 0211-Design-Add-and-Search-Words-Data-Structure/main.go
@@ -1,9 +1,13 @@
 package main
 
-import "strings"
-
 type WordDictionary struct {
-	m map[string]struct{}
+	m        map[string]struct{}
+	children [26]*Node
+}
+
+type Node struct {
+	children [26]*Node
+	hasEnd   bool
 }
 
 func Constructor() WordDictionary {
@@ -14,18 +18,57 @@ func Constructor() WordDictionary {
 
 func (this *WordDictionary) AddWord(word string) {
 	this.m[word] = struct{}{}
+	if this.children[word[0]-97] == nil {
+		this.children[word[0]-97] = &Node{}
+	}
+	this.children[word[0]-97].AddWord(word[1:])
+}
+
+func (n *Node) AddWord(word string) {
+	if word == "" {
+		n.hasEnd = true
+		return
+	}
+	if n.children[word[0]-97] == nil {
+		n.children[word[0]-97] = &Node{}
+	}
+	n.children[word[0]-97].AddWord(word[1:])
 }
 
 func (this *WordDictionary) Search(word string) bool {
 	if _, ok := this.m[word]; ok {
 		return true
 	}
-	if strings.ContainsRune(word, '.') {
-		for i := 0; i < 26; i++ {
-			alph := string([]byte{(byte)(97 + i)})
-			if this.Search(strings.Replace(word, ".", alph, 1)) {
+	w := word[0]
+	if w != '.' {
+		if this.children[w-97] != nil {
+			return this.children[w-97].Search(word[1:])
+		}
+		return false
+	}
+	if w == '.' {
+		for _, c := range this.children {
+			if c != nil && c.Search(word[1:]) {
 				return true
 			}
+		}
+	}
+	return false
+}
+
+func (n *Node) Search(word string) bool {
+	if word == "" {
+		return n.hasEnd
+	}
+	if word[0] != '.' {
+		if n.children[word[0]-97] == nil {
+			return false
+		}
+		return n.children[word[0]-97].Search(word[1:])
+	}
+	for _, c := range n.children {
+		if c != nil && c.Search(word[1:]) {
+			return true
 		}
 	}
 	return false
--- 0211-Design-Add-and-Search-Words-Data-Structure/main_test.go
+++ 0211-Design-Add-and-Search-Words-Data-Structure/main_test.go
@@ -20,3 +20,36 @@ func TestWordDictionary(t *testing.T) {
 		t.Errorf("should true")
 	}
 }
+
+func TestWordDictionary2(t *testing.T) {
+	wd := Constructor()
+	wd.AddWord("at")
+	wd.AddWord("and")
+	wd.AddWord("an")
+	wd.AddWord("add")
+	if wd.Search("a") {
+		t.Errorf("should false")
+	}
+	if wd.Search(".at") {
+		t.Errorf("should false")
+	}
+	wd.AddWord("bat")
+	if !wd.Search(".at") {
+		t.Errorf("should true")
+	}
+	if !wd.Search("an.") {
+		t.Errorf("should true")
+	}
+	if wd.Search("a.d.") {
+		t.Errorf("should false")
+	}
+	if wd.Search("b.") {
+		t.Errorf("should false")
+	}
+	if !wd.Search("a.d") {
+		t.Errorf("should true")
+	}
+	if wd.Search(".") {
+		t.Errorf("should false")
+	}
+}
