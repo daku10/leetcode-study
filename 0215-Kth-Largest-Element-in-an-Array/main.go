0fa325e60c292d2b88b3766d88447571cdde5b35
daku10 <daku10.dev@gmail.com>
2023-07-15 22:12:22 +0900

    0215 without sort, but too slow

--- 0215-Kth-Largest-Element-in-an-Array/main.go
+++ 0215-Kth-Largest-Element-in-an-Array/main.go
@@ -1,12 +1,43 @@
 package main
 
-import (
-	"sort"
-)
-
 func findKthLargest(nums []int, k int) int {
-	sort.Slice(nums, func(i, j int) bool {
-		return nums[i] > nums[j]
-	})
-	return nums[k-1]
+	if len(nums) < 1000 {
+		return bruteforce(nums, k)
+	}
+	var equalOrLarge []int
+	var small []int
+	pivot := nums[0]
+	for i := 1; i < len(nums); i++ {
+		if pivot > nums[i] {
+			small = append(small, nums[i])
+		} else {
+			equalOrLarge = append(equalOrLarge, nums[i])
+		}
+	}
+	if len(equalOrLarge) == k-1 {
+		return pivot
+	}
+	if len(equalOrLarge) >= k {
+		return findKthLargest(equalOrLarge, k)
+	}
+	return findKthLargest(small, k-len(equalOrLarge)-1)
+}
+
+func bruteforce(nums []int, k int) int {
+	var result int
+	for i := 0; i < k; i++ {
+		max := nums[0]
+		maxIndex := 0
+		for j := 0; j < len(nums)-i; j++ {
+			if max < nums[j] {
+				max = nums[j]
+				maxIndex = j
+			}
+		}
+		result = max
+		tmp := nums[maxIndex]
+		nums[maxIndex] = nums[len(nums)-i-1]
+		nums[len(nums)-i-1] = tmp
+	}
+	return result
 }
