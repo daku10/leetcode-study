a933b778789db0cde6fc06eac596714db8b4a3d1
daku10 <daku10.dev@gmail.com>
2023-07-25 12:15:27 +0900

    0221 optimize DP

--- 0221-Maximal-Square/main.go
+++ 0221-Maximal-Square/main.go
@@ -3,59 +3,25 @@ package main
 func maximalSquare(matrix [][]byte) int {
 	width := len(matrix[0])
 	height := len(matrix)
-	dp := make([][]int, height)
-	for y := 0; y < height; y++ {
-		dp[y] = make([]int, width)
-		for x := 0; x < width; x++ {
-			dp[y][x] = -1
-		}
+	result := 0
+	dp := make([][]int, height+1)
+	for y := 0; y < height+1; y++ {
+		dp[y] = make([]int, width+1)
 	}
 
 	for y := 0; y < height; y++ {
 		for x := 0; x < width; x++ {
-			if dp[y][x] == -1 {
-				calcDP(matrix, &dp, x, y)
+			if matrix[y][x] == '0' {
+				continue
+			}
+			dp[y+1][x+1] = min(min(dp[y][x], dp[y][x+1]), dp[y+1][x]) + 1
+			if dp[y+1][x+1] > result {
+				result = dp[y+1][x+1]
 			}
 		}
 	}
 
-	max := 0
-	for y := 0; y < height; y++ {
-		for x := 0; x < width; x++ {
-			if dp[y][x] > max {
-				max = dp[y][x]
-			}
-		}
-	}
-	return max * max
-}
-
-func calcDP(matrix [][]byte, dp *[][]int, x int, y int) {
-	if matrix[y][x] == '0' {
-		(*dp)[y][x] = 0
-		return
-	}
-	width := len(matrix[0])
-	height := len(matrix)
-	if x+1 == width {
-		(*dp)[y][x] = 1
-		return
-	}
-	if y+1 == height {
-		(*dp)[y][x] = 1
-		return
-	}
-	if (*dp)[y][x+1] == -1 {
-		calcDP(matrix, dp, x+1, y)
-	}
-	if (*dp)[y+1][x] == -1 {
-		calcDP(matrix, dp, x, y+1)
-	}
-	if (*dp)[y+1][x+1] == -1 {
-		calcDP(matrix, dp, x+1, y+1)
-	}
-
-	(*dp)[y][x] = min(min((*dp)[y][x+1], (*dp)[y+1][x]), (*dp)[y+1][x+1]) + 1
+	return result * result
 }
 
 func min(x, y int) int {
