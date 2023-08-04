4d9804a483c4f0a0c568f55a165e00dc735feef8
daku10 <daku10.dev@gmail.com>
2023-08-04 20:36:18 +0900

    0227 use one variable

--- 0227-Basic-Calculator-II/main.go
+++ 0227-Basic-Calculator-II/main.go
@@ -5,85 +5,60 @@ import (
 	"strings"
 )
 
-type stack struct {
-	m []int
-}
-
-func (s *stack) push(x int) {
-	s.m = append(s.m, x)
-}
-
-func (s *stack) pop() int {
-	r := s.m[len(s.m)-1]
-	s.m = s.m[:len(s.m)-1]
-	return r
-}
-
-func (s *stack) raw() []int {
-	return s.m
-}
-
 func calculate(s string) int {
-	stack := stack{}
 	var current strings.Builder
+	result := 0
+	lastNumber := 0
 	var operator byte = ' '
 	for i := 0; i < len(s); i++ {
 		switch s[i] {
 		case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
 			current.WriteByte(s[i])
-		case '+':
-			stack.push(toInt(current.String()))
-			current.Reset()
-			calc(&stack, operator)
-			operator = '+'
-		case '-':
-			stack.push(toInt(current.String()))
-			current.Reset()
-			calc(&stack, operator)
-			operator = '-'
-		case '*':
-			stack.push(toInt(current.String()))
-			current.Reset()
-			calc(&stack, operator)
-			operator = '*'
-		case '/':
-			stack.push(toInt(current.String()))
-			current.Reset()
-			calc(&stack, operator)
-			operator = '/'
+		case '+', '-', '*', '/':
+			switch operator {
+			case '+':
+				result += lastNumber
+				lastNumber = toInt(current.String())
+				current.Reset()
+			case '-':
+				result += lastNumber
+				lastNumber = -1 * toInt(current.String())
+				current.Reset()
+			case '*':
+				lastNumber *= toInt(current.String())
+				current.Reset()
+			case '/':
+				lastNumber /= toInt(current.String())
+				current.Reset()
+			default:
+				lastNumber = toInt(current.String())
+				current.Reset()
+			}
+			operator = s[i]
 		default:
 		}
 	}
-	stack.push(toInt(current.String()))
-	calc(&stack, operator)
-	result := 0
-	for _, r := range stack.raw() {
-		result += r
+	switch operator {
+	case '+':
+		result += lastNumber
+		lastNumber = toInt(current.String())
+		current.Reset()
+	case '-':
+		result += lastNumber
+		lastNumber = -1 * toInt(current.String())
+		current.Reset()
+	case '*':
+		lastNumber *= toInt(current.String())
+		current.Reset()
+	case '/':
+		lastNumber /= toInt(current.String())
+		current.Reset()
+	default:
+		lastNumber = toInt(current.String())
+		current.Reset()
 	}
+	result += lastNumber
 	return result
-}
-
-func calc(s *stack, operator byte) {
-	if operator == ' ' {
-		return
-	}
-	if operator == '+' {
-		return
-	}
-	if operator == '-' {
-		r := s.pop()
-		s.push(r * -1)
-	}
-	if operator == '*' {
-		r := s.pop()
-		t := s.pop()
-		s.push(t * r)
-	}
-	if operator == '/' {
-		r := s.pop()
-		t := s.pop()
-		s.push(t / r)
-	}
 }
 
 func toInt(s string) int {
