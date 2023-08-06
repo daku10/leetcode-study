4e884b1e34361d89a0ceb12df20058795f9f61be
daku10 <daku10.dev@gmail.com>
2023-08-06 19:50:20 +0900

    0229 use Boyer-Moore marjority vote algorithm

--- 0229-Majority-Element-II/main.go
+++ 0229-Majority-Element-II/main.go
@@ -1,16 +1,56 @@
 package main
 
+import "math"
+
 func majorityElement(nums []int) []int {
-	memo := make(map[int]int)
+	if len(nums) == 1 {
+		return nums
+	}
+
+	candidateOne := math.MinInt
+	candidateTwo := math.MinInt
+	oneNumber := 0
+	twoNumber := 0
+
 	for _, num := range nums {
-		memo[num]++
+		if candidateOne == num {
+			oneNumber++
+			continue
+		}
+		if candidateTwo == num {
+			twoNumber++
+			continue
+		}
+		if oneNumber == 0 {
+			candidateOne = num
+			oneNumber++
+			continue
+		}
+		if twoNumber == 0 {
+			candidateTwo = num
+			twoNumber++
+			continue
+		}
+		oneNumber--
+		twoNumber--
 	}
 
 	var result []int
-	for k, v := range memo {
-		if v > len(nums)/3 {
-			result = append(result, k)
+	oneCount := 0
+	twoCount := 0
+	for _, num := range nums {
+		if num == candidateOne {
+			oneCount++
+		} else if num == candidateTwo {
+			twoCount++
 		}
 	}
+	if oneCount > len(nums)/3 {
+		result = append(result, candidateOne)
+	}
+	if twoCount > len(nums)/3 {
+		result = append(result, candidateTwo)
+	}
+
 	return result
 }
