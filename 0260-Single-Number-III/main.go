ee069e654ce072a85590894416207a0f5a45c0a4
daku10 <daku10.dev@gmail.com>
2024-02-12 13:21:35 +0900

    0260 use only constant extra space

--- 0260-Single-Number-III/main.go
+++ 0260-Single-Number-III/main.go
@@ -1,15 +1,21 @@
 package main
 
 func singleNumber(nums []int) []int {
-	memo := make(map[int]int, len(nums))
-	for _, n := range nums {
-		memo[n]++
+	var diff int
+	for _, num := range nums {
+		diff ^= num
 	}
-	var res []int
-	for k, v := range memo {
-		if v == 1 {
-			res = append(res, k)
+	rightmost := 1
+	for diff&rightmost == 0 {
+		rightmost <<= 1
+	}
+	var a, b int
+	for _, num := range nums {
+		if num&rightmost == 0 {
+			a ^= num
+		} else {
+			b ^= num
 		}
 	}
-	return res
+	return []int{a, b}
 }
