4cdcce61ad715c9336bb8bda3dffca3c1ab31ded
daku10 <daku10.dev@gmail.com>
2024-02-15 21:32:16 +0900

    0283 improve speed

--- 0283-Move-Zeroes/main.go
+++ 0283-Move-Zeroes/main.go
@@ -1,13 +1,18 @@
 package main
 
 func moveZeroes(nums []int) {
+	j := -1
 	for i := 0; i < len(nums); i++ {
 		if nums[i] == 0 {
-			for j := i + 1; j < len(nums); j++ {
+			if j == -1 {
+				j = i + 1
+			}
+			for j < len(nums) {
 				if nums[j] != 0 {
 					nums[i], nums[j] = nums[j], nums[i]
 					break
 				}
+				j++
 			}
 		}
 	}
