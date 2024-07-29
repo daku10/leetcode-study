8cd2d9df4cfcb158c00d923f2839af85b1be84a1
daku10 <daku10.dev@gmail.com>
2024-07-29 23:00:56 +0900

    0495 shortify answer

--- 0495-Teemo-Attacking/main.go
+++ 0495-Teemo-Attacking/main.go
@@ -1,14 +1,12 @@
 package main
 
 func findPoisonedDuration(timeSeries []int, duration int) int {
+	if duration == 0 {
+		return 0
+	}
 	var result int
 	for i := 0; i < len(timeSeries)-1; i++ {
-		t := timeSeries[i+1] - timeSeries[i]
-		if t >= duration {
-			result += duration
-		} else {
-			result += t
-		}
+		result += min(duration, timeSeries[i+1]-timeSeries[i])
 	}
 	return result + duration
 }
